8b2b9861ff162e48214f6fc9ccb636858497adc6
Jonathon Tufts <86862338+jonathontufts@users.noreply.github.com>
2022-05-18 17:27:55 -0600

    final phases

--- app/models.py
+++ app/models.py
@@ -13,13 +13,40 @@ class Employee(db.Model, UserMixin):
    employee_number = db.Column(db.Integer, nullable=False, unique=True)
    hashed_password = db.Column(db.String(255), nullable=False)
 
-@property
-def password(self):
-   return self.hashed_password
 
-@password.setter
-def password(self, password):
-   self.hashed_password = generate_password_hash(password)
+   @property
+   def password(self):
+      return self.hashed_password
 
-def check_password(self, password):
-   return check_password_hash(self.password, password)
+   @password.setter
+   def password(self, password):
+      self.hashed_password = generate_password_hash(password)
+
+   def check_password(self, password):
+      return check_password_hash(self.password, password)
+
+
+class Menu(db.Model):
+   __tablename__ = 'menus'
+
+   id = db.Column(db.Integer, primary_key=True)
+   name = db.Column(db.String(30), nullable=False)
+   items = db.relationship('MenuItem', back_populates = 'menu', cascade="all, delete")
+
+class MenuItem(db.Model):
+   __tablename__ = 'items'
+
+   id = db.Column(db.Integer, primary_key=True)
+   name = db.Column(db.String(50), nullable=False)
+   price = db.Column(db.Float, nullable=False)
+   menu_id = db.Column(db.Integer, db.ForeignKey('menus.id'), nullable=False)
+   menus_type_id = db.Column(db.Integer, db.ForeignKey('menu_item_types.id'), nullable=False)
+   menu = db.relationship('Menu', back_populates= 'items')
+   
+
+
+class MenuItemType(db.Model):
+   __tablename__ = 'menu_item_types'
+
+   id = db.Column(db.Integer, primary_key=True)
+   name = db.Column(db.String(20), nullable=False)
